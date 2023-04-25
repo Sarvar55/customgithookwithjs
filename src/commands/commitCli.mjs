6ea923415d781423eab3d5d3bb5731d70e664e62
Sarvar55 <sarvar.musazade@gmail.com>
2023-04-25 21:32:12 +0300

    🐞 fix: commitCli.js içindeki kodlar duzenlendi

--- src/commands/commitCli.mjs
+++ src/commands/commitCli.mjs
@@ -2,59 +2,64 @@ import { outro, isCancel } from '@clack/prompts';
 import chalk from 'chalk';
 import { isConfirm } from '../util/confirim.mjs';
 import {
-    addFilesToStaged,
-    getChangedFiles,
-    getCommitSubject,
-    getStatus,
-    gitCommit
+  addFilesToStaged,
+  getChangedFiles,
+  getCommitSubject,
+  getStatus,
+  gitCommit
 } from '../util/git.js';
 import { commitTypesWithEmoji, commitTypes } from '../util/CommitType.mjs';
 import cliSelect from 'cli-select';
 
-export const commit = async() => {
-    const emojiEnabled = await isConfirm(
-        'Do you want to add emoji to commit message?'
-    );
+export const commit = async () => {
+  const changedFiles = await getChangedFiles();
+  if (changedFiles.length === 0) {
+    outro(`${chalk.red('✖')} commit mesajı iptal edildi.`);
+    process.exit(1);
+  }
 
-    const { value: commitType } = await cliSelect({
-        values: emojiEnabled ? commitTypesWithEmoji : commitTypes,
-        valueRenderer: (value, selected) => {
-            if (selected) {
-                return chalk.underline(value);
-            }
-            return value;
-        }
-    });
+  const emojiEnabled = await isConfirm(
+    'Do you want to add emoji to commit message?'
+  );
 
-    const commitSubject = await getCommitSubject();
-
-    const message = `${commitType}: ${commitSubject}`;
-
-    try {
-        const isConfirmedCommit = await isConfirm('Commit mesajını onaylayın?');
-
-        if (!isConfirmedCommit || isCancel(isConfirmedCommit)) {
-            outro(`${chalk.red('✖')} commit mesajı iptal edildi.`);
-            process.exit(1);
-        }
-
-        const statusOutput = await getStatus();
-        const changedFiles = await getChangedFiles();
-
-        if (statusOutput.trim() === '' || changedFiles.length === 0) {
-            outro(`${chalk.red('✖')} commit için herhangi bir değişiklik yok.`);
-            process.exit(1);
-        }
-
-        await addFilesToStaged(changedFiles);
-
-        const commitOutput = await gitCommit(message);
-        outro(`${chalk.green('✔')} commit başarılı.`);
-        outro(commitOutput);
-
-        return true;
-    } catch (err) {
-        outro(`${chalk.red('✖')} ${err.message}`);
-        process.exit(1);
+  const { value: commitType } = await cliSelect({
+    values: emojiEnabled ? commitTypesWithEmoji : commitTypes,
+    valueRenderer: (value, selected) => {
+      if (selected) {
+        return chalk.underline(value);
+      }
+      return value;
     }
+  });
+
+  const commitSubject = await getCommitSubject();
+
+  const message = `${commitType}: ${commitSubject}`;
+
+  try {
+    const isConfirmedCommit = await isConfirm('Commit mesajını onaylayın?');
+
+    if (!isConfirmedCommit || isCancel(isConfirmedCommit)) {
+      outro(`${chalk.red('✖')} commit mesajı iptal edildi.`);
+      process.exit(1);
+    }
+
+    const statusOutput = await getStatus();
+
+    if (statusOutput.trim() === '') {
+      outro(`${chalk.red('✖')} commit için herhangi bir değişiklik yok.`);
+      process.exit(1);
+    }
+
+    await addFilesToStaged(changedFiles);
+
+    const commitOutput = await gitCommit(message);
+    outro(`${chalk.green('✔')} commit başarılı.`);
+    outro(commitOutput);
+
+    return true;
+  } catch (err) {
+    outro(`${chalk.red('✖')} ${err.message}`);
+    process.exit(1);
+  }
 };
