f29cdc162bc88092a553b874bff2da231b36fe52
Sarvar55 <sarvar.musazade@gmail.com>
2023-04-25 21:29:57 +0300

    🔨 refactor: app.js içinde olan gereksiz kodlar silindi

--- app.js
+++ app.js
@@ -1,32 +1,3 @@
 import { cli } from './src/cli/index.mjs';
 
-// import Configstore from 'configstore';
-// import yargs from 'yargs';
 cli();
-// const config = new Configstore();
-// console.log('sdjkfkjsnksnkgn');
-// console.log(config.get('emoji'));
-
-// import { hideBin } from 'yargs/helpers';
-
-// yargs(hideBin(process.argv))
-//   .command(
-//     'set-emoji [flag]',
-//     'set emoji',
-//     (yargs) => {
-//       return yargs.positional('flag', {
-//         default: false
-//       });
-//     },
-//     (argv) => {
-//       if (argv.verbose) console.info(`start server on :${argv.port}`);
-//       const config = new Configstore();
-//       config.set('emoji', argv.flag);
-//     }
-//   )
-//   .option('verbose', {
-//     alias: 'v',
-//     type: 'boolean',
-//     description: 'Run with verbose logging'
-//   })
-//   .parse();
