71707dab963c371eb236b795961ce450d2b2d77b
Sarvar55 <sarvar.musazade@gmail.com>
2023-06-09 20:44:12 +0300

    ✨ feat: artık istedigin branca da push yapa bilirsin

--- src/commands/pushCli.mjs
+++ src/commands/pushCli.mjs
@@ -8,25 +8,29 @@ export const push = async () => {
   const shouldPushToBranch = await isConfirm(
     'Is there a branch you want to `push specifically`?'
   );
-  let selectedBranch = '';
-  if (shouldPushToBranch && !isCancel(shouldPushToBranch)) {
-    const branches = await getLocalBranches();
-    selectedBranch = await customCliSelect(branches);
-  }
-  if (isPushConfirmed && !isCancel(isPushConfirmed)) {
-    const pushProgress = spinner();
+  try {
+    let selectedBranch = '';
+    if (shouldPushToBranch && !isCancel(shouldPushToBranch)) {
+      const branches = await getLocalBranches();
+      selectedBranch = await customCliSelect(branches);
+    }
+    if (isPushConfirmed && !isCancel(isPushConfirmed)) {
+      const pushProgress = spinner();
 
-    pushProgress.start('push işlemi gerceklesiyor');
+      pushProgress.start('push işlemi gerceklesiyor');
 
-    const origin = await getGitRemoteUrl();
-    const stdout = await gitPush(origin, selectedBranch);
+      const origin = await getGitRemoteUrl();
+      const stdout = await gitPush(origin, selectedBranch);
 
-    pushProgress.stop(
-      `${chalk.green('✔')} successfully pushed all commits to ${origin}`
-    );
+      pushProgress.stop(
+        `${chalk.green('✔')} successfully pushed all commits to ${origin}`
+      );
 
-    if (stdout) outro(stdout);
-  } else {
-    outro(`${chalk.red('✖')} push  canceled`);
+      if (stdout) outro(stdout);
+    } else {
+      outro(`${chalk.red('✖')} push  canceled`);
+    }
+  } catch (error) {
+    outro(`${chalk.red('✖')} push error: ${error.message}`);
   }
 };
