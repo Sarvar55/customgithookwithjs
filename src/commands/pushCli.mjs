f125bfbb312cd83b6b2192e6783e216038bd2aa2
Sarvar55 <sarvar.musazade@gmail.com>
2024-02-19 23:14:01 +0300

    docs: updated pushCli.mjs

--- src/commands/pushCli.mjs
+++ src/commands/pushCli.mjs
@@ -5,7 +5,15 @@ import chalk from 'chalk';
 
 export const push = async () => {
   const isPushConfirmed = await isConfirm('Do you want to run `git push`?');
+  const shouldPushToBranch = await isConfirm(
+    'Is there a branch you want to `push specifically`?'
+  );
   try {
+    let selectedBranch = '';
+    if (shouldPushToBranch && !isCancel(shouldPushToBranch)) {
+      const branches = await getLocalBranches();
+      selectedBranch = await customCliSelect(branches);
+    }
     if (isPushConfirmed && !isCancel(isPushConfirmed)) {
       const pushProgress = spinner();
 
